7553e5ee2c36087b885e3696b04713113ab01678
Alexander Grenkov <a.grenkov@gmail.com>
2024-04-09 01:18:03 +0500

    Add name and namespace

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from myapp.views import index, index_item, contacts
 
 
+app_name = 'myapp'
+
 urlpatterns = [
     path('hello/', index),
-    path('hello/<int:my_id>/', index_item),
+    path('hello/<int:my_id>/', index_item, name='detail'),
     path('contacts/', contacts),
 ]
 
