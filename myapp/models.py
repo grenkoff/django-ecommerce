cbfc8b00228540b80ddbbd5e04de9048bb2032ce
Alexander Grenkov <a.grenkov@gmail.com>
2024-04-09 19:27:10 +0500

    Add image in models

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, default='')
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200, default='')
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.name
