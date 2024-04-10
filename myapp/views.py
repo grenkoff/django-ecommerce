ff40e1cf4419ebc20c5b4effbd82ecaceedb19e7
Alexander Grenkov <a.grenkov@gmail.com>
2024-04-11 04:35:45 +0500

    Fix

--- myapp/views.py
+++ myapp/views.py
@@ -46,3 +46,7 @@ def delete_item(request, my_id):
         return redirect("/myapp/")
     context = {'item': item}
     return render(request, "myapp/deleteitem.html", context)
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
