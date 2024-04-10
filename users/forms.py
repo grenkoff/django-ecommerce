7a0b6b38d04fd93f57ada923954715233a1b9651
Alexander Grenkov <a.grenkov@gmail.com>
2024-04-10 19:15:45 +0500

    Edit forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,39 @@ from django import forms
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    username = forms.CharField(required=True)
-    password1 = forms.CharField(required=True)
-    password2 = forms.CharField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={"class": "focus:outline-none", "placeholder": "mail@mail.com"}
+        ),
+    )
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "focus:outline-none", "placeholder": "Enter username..."}
+        ),
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "focus:outline-none", "placeholder": "Enter password"}
+        ),
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "focus:outline-none", "placeholder": "Enter same password"}
+        ),
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+    # def save(self, commit=True):
+    #     user = super(NewUserForm, self).save(commit=False)
+    #     user.email = self.cleaned_data['email']
+    #     if commit:
+    #         user.save()
+    #     return user
